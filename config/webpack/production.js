4da7d69fbade8a7bdb2b9158706178eb14d3c39e
kosukeKK <kosuke.kato@jmty.jp>
2017-12-15 20:29:02 +0900

    front

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,8 @@
 const environment = require('./environment')
 
+environment.loaders.set('typescript', {
+    test: /\.tsx?$/,
+    use: 'awesome-typescript-loader'
+})
+
 module.exports = environment.toWebpackConfig()
